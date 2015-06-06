e9695a6ebbb044436156ecbabc47f0fe21d2162f
Wmaxlees <william.lees@ucdenver.edu>
2015-06-05 20:53:20 -0600

    Added Autodoc comments to test

---
+++ test/test.cpp
@@ -0,0 +1,11 @@
+/**
+   FILE: test.cpp
+
+   @author W. Max Lees
+   @docdate 06.05.2015
+
+   @descr Main test file for Catch. Only has the #define line
+*/
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
