f6abcc6afaeff148ee8aef5c901209770ca18325
Wmaxlees <william.lees@ucdenver.edu>
2015-06-06 21:56:21 -0600

    Added LICENSE

--- jarvis-nlp/src/main.cpp
+++ jarvis-nlp/src/main.cpp
@@ -4,10 +4,13 @@
 */
 
 #include <cstdlib>
+#include <iostream>
 
 #include "SigmoidNeuron.hpp"
 
 int main(int argc, char** argv) {
+
+	std::cout << "Stub----" << std::endl;
 
 	return EXIT_SUCCESS;
 }
