d595eb909a6555c695baa10b22436d2caa8353c4
Wmaxlees <william.lees@ucdenver.edu>
2015-06-06 22:50:52 -0600

    Added stub for B+ Neuron Factory

---
+++ jarvis-nlp/src/SigmoidNeuronBPlusFactory.cpp
@@ -0,0 +1,30 @@
+/**
+	Copyright 2015 W. Max Lees
+
+	This file is part of Jarvis OS.
+
+	Jarvis OS is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Jarvis OS is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+	FILE: SigmoidNeuronBPlusFactory.cpp
+
+	@author W. Max Lees
+	@docdate 06.06.2015
+
+	@descr Class that generates B+ Sigmoid Neurons
+*/
+
+#include "SigmoidNeuronBPlusFactory.hpp"
+
