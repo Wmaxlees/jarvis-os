5fbda358d111d11e38c9f8f73d2fa408ea8bbe26
W. Max Lees <william.lees@echostar.com>
2015-06-11 09:28:50 -0600

    Created very basic header for JarvisNLP class

---
+++ jarvis-nlp/src/JarvisNLP.hpp
@@ -0,0 +1,47 @@
+/**
+   Copyright 2015 W. Max Lees
+
+   This file is part of Jarvis OS.
+
+   Jarvis OS is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   Jarvis OS is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Jarvis OS.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+   FILE: JarvisNLP.hpp
+
+   @author W. Max Lees
+   @docdate 06.11.2015
+
+   @descr The header file for the class that will run the entire
+   		  program
+*/
+
+#ifndef _H_JARVISNLP
+#define	_H_JARVISNLP
+
+class JarvisNLP {
+	public:
+		
+
+	protected:
+
+
+};
+
+#endif
+
+
+
+
+
